38fbc0d682f56b7fe2f521129d1b094183b864a9
Aritra Basak <104417333+AritraBasakCode@users.noreply.github.com>
2026-03-20 13:58:37 +0530

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("analyzeBtn").addEventListener("click", () => {
-  // Future: validate inputs, upload resume, call extraction + ML APIs, store results
-  window.location.href = "skill-gap.html";
-});
